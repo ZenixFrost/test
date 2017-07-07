0385920e8412b7ab87f0df6bdbed357374697d00
Thanadetch <thanadetch.l@cdg.co.th>
2017-07-07 21:16:27 +0700

    test3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
+import { CustomerComponent } from './customer/customer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    CustomerComponent
   ],
   imports: [
     BrowserModule
